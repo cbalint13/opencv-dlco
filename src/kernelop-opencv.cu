ba9741dbd8c62ec41dfb14898b859ec1eebb2528
Balint Cristian <cristian.balint@gmail.com>
2016-02-22 20:55:59 +0200

    Update kernelop-opencv.cu

--- src/kernelop-opencv.cu
+++ src/kernelop-opencv.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014  Balint Cristian (cristian dot balint at gmail dot com)
+ * Copyright (c) 2015  Balint Cristian (cristian dot balint at gmail dot com)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
