260f7001bd030a2a7d67d1ee5412e716420c2441
Balint Cristian <cristian.balint@gmail.com>
2016-03-03 10:10:22 +0200

    Add pj-learn results.

--- workspace/08-pjlearn.sh
+++ workspace/08-pjlearn.sh
@@ -7,7 +7,7 @@
 for f in `find . -name *-unproj.h5 | sed -e 's|\.\/||g' -e 's|-unproj.h5||g'`
   do
 
-  for mu in 0.0010 0.0020 0.0030
+  for mu in 0.0005 0.0010 0.0020 0.0030
     do
 
     for gamma in 0.050 0.250 0.500 1.000
